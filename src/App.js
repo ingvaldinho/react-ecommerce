d91e874ec4780cfcaa16b250ee5ed0d8aadd9e4d
Ingvald <ingvald.orbeck@gmail.com>
2019-11-15 18:19:26 +0100

    added redux and implemented userReducer and UserActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
+import {connect} from 'react-redux'
+
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './component/header/header.component';
-import {auth, createUserprofileDocument} from './firebase/firebase.utils';
 
+import {auth, createUserprofileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
  
 
 class App extends React.Component{
-  constructor(){
-    super();
 
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unSubsribeFromAuth= null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unSubsribeFromAuth =  auth.onAuthStateChanged(async userAuth=>{
      
       if(userAuth){
@@ -28,16 +26,14 @@ class App extends React.Component{
          console.log(userRef);
         
         userRef.onSnapshot(snapshot=>{
-          this.setState({
-            currentUser:{
+          setCurrentUser({           
               id: snapshot.id,
-              ...snapshot.data()
-            }
+              ...snapshot.data()            
           }) 
           console.log(this.state);
         })
       }else{
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
       }
       
 
@@ -54,7 +50,7 @@ class App extends React.Component{
   render(){
     return (
       <div className="homepage">
-        <Header currentUser={this.state.currentUser} />
+        <Header/>
         <Switch>              
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -66,4 +62,8 @@ class App extends React.Component{
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user)) 
+})
+
+export default connect(null,mapDispatchToProps)(App);
--- src/component/sign-in/sign-in.component.jsx
+++ src/component/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {signInWithGoogle,auth} from '../../firebase/firebase.utils';
 
 
 class SignIn extends Component{
@@ -19,10 +19,16 @@ class SignIn extends Component{
 
    
 
-    handleSubmit= (event)=>{
+    handleSubmit= async (event)=>{
         event.preventDefault();
+        const {email,password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({'email': '', 'password':''});
 
-        this.setState({'email': '', 'password':''});
+        } catch (error) {
+            console.error(error);
+        }
 
     }
 
