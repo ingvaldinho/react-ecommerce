d154e6098b99849ac7601709f15637be603c302d
Ingvald <ingvald.orbeck@gmail.com>
2020-01-06 19:55:46 +0100

    modify css of collection item,header, collection preview and signup sign in, add media query

--- client/src/component/header/header.styles.jsx
+++ client/src/component/header/header.styles.jsx
@@ -9,12 +9,23 @@ export const HeaderContainer = styled.div`
     display: flex;
     justify-content: space-between;
     margin-bottom: 25px;
+
+    @media screen and (max-width: 800px) {
+        height: 60px;
+        padding: 10px;
+        margin-bottom:20px
+    }
 `;
 
 export const LogoContainer = styled(Link)`
     height: 100%;
     width: 70px;
     padding: 25px;
+
+    @media screen and (max-width: 800px) {
+        width: 50px;
+        padding: 0;
+    }
 `;
 
 export const OptionContainer = styled.div`
@@ -23,15 +34,21 @@ export const OptionContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    @media screen and (max-width: 800px) {
+        width:80%;
+    }
+
 `;
 
 export const OptionLink = styled(Link)`
     padding: 10px 15px;
     cursor: pointer;
+
 `
 export const OptionDiv = styled.div`
     padding: 10px 15px;
     cursor: pointer;
+
 `
 
 
